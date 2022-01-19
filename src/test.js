fdfff8e9b5713f815c948ea4377d8957c899c023
JUB0T <jforeverything2007@gmail.com>
2022-01-19 16:22:23 +0100

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,6 +1,6 @@
 const GREPPER = require("./index")
 
 async function __main__() {
-    Data = await GREPPER.getPrivacySettings("618fn9ln6bmvhsmct8ri9lp4l5", true);
+    Data = await GREPPER.getPrivacySettings("", true);
     console.log(Data)
 } __main__()
